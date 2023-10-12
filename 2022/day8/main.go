17c870bbd1f6dcd4b84b46d3aff59cab54a9d0bb
Andrés Radunsky <aradunsky@mulesoft.com>
2023-10-12 13:19:43 -0300

    day8 part 2

--- 2022/day8/main.go
+++ 2022/day8/main.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+type TreeView struct {
+	L, R, U, D int
+}
+
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 
@@ -28,6 +32,11 @@ func main() {
 
 	count := 0
 
+	viewD := make([][]TreeView, len(m))
+	for i := range viewD {
+		viewD[i] = make([]TreeView, len(m[0]))
+	}
+
 	for i := range m {
 		maxH := -1
 		for j := len(m[i]) - 1; j >= 0; j-- {
@@ -37,6 +46,19 @@ func main() {
 					m[i][j][1] += 1
 				}
 				maxH = m[i][j][0]
+			}
+
+			if j < len(m[0])-1 {
+				curT := m[i][j][0]
+				v := 1
+				for nextT := j + v; nextT < len(m[0])-1; nextT = j + v {
+					if m[i][nextT][0] < curT {
+						v += viewD[i][nextT].R
+					} else {
+						break
+					}
+				}
+				viewD[i][j].R = v
 			}
 		}
 	}
@@ -51,6 +73,19 @@ func main() {
 				}
 				maxH = m[i][j][0]
 			}
+
+			if j > 0 {
+				curT := m[i][j][0]
+				v := 1
+				for nextT := j - v; nextT > 0; nextT = j - v {
+					if m[i][nextT][0] < curT {
+						v += viewD[i][nextT].L
+					} else {
+						break
+					}
+				}
+				viewD[i][j].L = v
+			}
 		}
 	}
 
@@ -63,6 +98,19 @@ func main() {
 					m[j][i][1] += 1
 				}
 				maxH = m[j][i][0]
+			}
+
+			if j > 0 {
+				curT := m[j][i][0]
+				v := 1
+				for nextT := j - v; nextT > 0; nextT = j - v {
+					if m[nextT][i][0] < curT {
+						v += viewD[nextT][i].U
+					} else {
+						break
+					}
+				}
+				viewD[j][i].U = v
 			}
 		}
 	}
@@ -78,8 +126,32 @@ func main() {
 
 				maxH = m[j][i][0]
 			}
+
+			if j < len(m[0])-1 {
+				curT := m[j][i][0]
+				v := 1
+				for nextT := j + v; nextT < len(m[0])-1; nextT = j + v {
+					if m[nextT][i][0] < curT {
+						v += viewD[nextT][i].D
+					} else {
+						break
+					}
+				}
+				viewD[j][i].D = v
+			}
 		}
 	}
 
-	fmt.Println(count)
+	maxView := 0
+	for i := range viewD {
+		for j := range viewD[i] {
+			curView := viewD[i][j]
+			val := curView.D * curView.R * curView.U * curView.L
+			if val > maxView {
+				maxView = val
+			}
+		}
+	}
+	fmt.Println("PART 1: ", count)
+	fmt.Println("PART 2: ", maxView)
 }
